663ca114fd5200e126fd31fd20f36a44dc59fa52
Sitthan181 <68070181@kmitl.ac.th>
2026-07-08 11:01:49 +0700

    add folder and C file
    not done yet, lab02XX

---
+++ It all C/lab02-00.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello! My name is Sitthan Aromprasert\n");
+	return 0;
+}
+
---
+++ It all C/lab02-02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int inputNumber;
+	sacnf("%d", inputNumber);
+	printf("The input is %d", inputNumber);
+	return 0;
+}
+
