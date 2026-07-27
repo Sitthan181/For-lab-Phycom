8bed916b446fb5563f0688793c4e97adf510ffdd
Sitthan181 <68070181@kmitl.ac.th>
2026-07-27 21:46:29 +0700

    *del same same file in lab3(same but dif name)

--- It_all_C/lab3/lab03-5_Perimeter-of-rectangle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    double width, length;
-    scanf("%lf\n%lf", &width, &length);
-    double sum = (width * 2) + (length * 2);
-    printf("Perimeter of rectangle = %.04lf units", sum);
-
-    return 0;
-}
